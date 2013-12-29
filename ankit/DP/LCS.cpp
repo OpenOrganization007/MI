61eff6a30098e5d5cc5fd13a745f99b8b4831bfe
ankit srivastava <srivastava.ankit.2012@gmail.com>
2013-12-30 00:15:37 +0530

    least common subsequence incomplete

---
+++ ankit/DP/LCS.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Hello World" << endl; 
+   
+   return 0;
+}
+char b[5][5],c [5][5];
+
+void LCSlength(char X[],char Y[],int m ,int n) {
+    
+}
+
+void printLCS(char b[][],char X[],int i,int j) {
+    
+}
